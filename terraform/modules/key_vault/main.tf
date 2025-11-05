8e8e25e73c6c5fc019161fcea1f5a9a1b4f48e8a
NathanD117 <72250703+NathanD98@users.noreply.github.com>
2025-11-05 23:22:39 +0000

    formatted with terraform format

--- terraform/modules/key_vault/main.tf
+++ terraform/modules/key_vault/main.tf
@@ -1,12 +1,12 @@
 # main.tf
 
 resource "azurerm_key_vault" "kv" {
-  name                     = var.key_vault_name
-  location                 = var.location
-  resource_group_name      = var.resource_group_name
-  sku_name                 = var.sku_name
+  name                        = var.key_vault_name
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  sku_name                    = var.sku_name
   enabled_for_disk_encryption = true
-  tenant_id                = data.azurerm_client_config.current.tenant_id
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
 
   # tags
   tags = {
--- terraform/modules/resource_group/main.tf
+++ terraform/modules/resource_group/main.tf
@@ -3,7 +3,7 @@
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
-    tags = {
+  tags = {
     "Project"     = var.project_name,
     "Environment" = var.environment,
     "ManagedBy"   = "Terraform" # Example of a fixed tag value
